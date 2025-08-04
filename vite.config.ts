cddbbcea5dcd598ef121acdf2721d3fdf1441f3d
Rekhta Menahil <rekhta674@gmail.com>
2025-08-04 21:38:06 +0500

    add tests coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
+  },
 })
