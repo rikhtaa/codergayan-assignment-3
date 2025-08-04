db4b3aa8ee7ed64bc0c85b50607f579f67d08955
Rekhta Menahil <rekhta674@gmail.com>
2025-08-04 19:00:58 +0500

    remove console

--- app.spec.tsx
+++ app.spec.tsx
@@ -4,6 +4,5 @@ import App from './src/App';
 
 test('renders hello world', () => {
   const result = ReactDOMServer.renderToString(<App/>)
-  console.log(result)
   expect(result).toContain('hello world!')
 });
